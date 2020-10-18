4083bad1ed225a4de3d55ed5861977073d2a39b0
johnpolacek <johnpolacek@gmail.com>
2020-10-18 15:20:26 -0500

    fix footer link format

--- components/ui/Footer.js
+++ components/ui/Footer.js
@@ -4,7 +4,10 @@ import { Box } from "theme-ui"
 const Footer = (props) => (
   <Box as="footer" sx={{ py: 6, textAlign: "center" }}>
     A project by <a href="https://johnpolacek.com/">John Polacek</a>. Read more{" "}
-    <Link href="/about">about it</Link>. Code on{" "}
+    <Link href="/about">
+      <a>about it</a>
+    </Link>
+    . Code on{" "}
     <a href="https://github.com/johnpolacek/trick-or-treat-it-forward">
       Github
     </a>
